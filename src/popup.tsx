1e5dd3831db8a587271c03458998058324a379a4
Alex <alexchen.info@gmail.com>
2022-11-26 00:16:06 +0800

    Add TypeScript

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,14 +1,14 @@
 import React from "react"
-import ReactDOM from "react-dom/client"
+import { createRoot }  from "react-dom/client"
 
 function Popup () {
 	return (
 		<div>
-			<h1>Hello world 2</h1>
+			<h1>Hello world 4</h1>
 			<p>This is a simple popup.</p>
 		</div>
 	)
 }
 
-const root = ReactDOM.createRoot(document.getElementById("react-target") as HTMLElement)
+const root = createRoot(document.getElementById("react-target")!)
 root.render(<Popup />)
