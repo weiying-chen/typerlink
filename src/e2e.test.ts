0635b4fe8fb1bf83739d90a9487353f5357c315f
Alex <alexchen.info@gmail.com>
2023-01-20 20:34:21 +0800

    Restore changes

--- src/e2e.test.ts
+++ src/e2e.test.ts
@@ -1,26 +1,8 @@
-// const puppeteer = require('puppeteer');
 import * as puppeteer from 'puppeteer';
-
-// (async () => {
-// 	// const browser = await puppeteer.launch();
-// 	const browser = await puppeteer.launch({
-// 		headless: false,
-// 		defaultViewport: null,
-// 	});
-// 	const page = await browser.newPage();
-
-// 	await page.goto('http://localhost:5500/dist/dummy.html');
-//   await page.type('#keys input', 'test');
-// 	await page.waitForTimeout(5000);
-// 	await browser.close();
-// })();
 
 describe('Puppeteer Test', () => {
 	let browser: puppeteer.Browser;
 	let page: puppeteer.Page;
-
-	// let browser;
-	// let page;
 
 	beforeAll(async () => {
 		browser = await puppeteer.launch({
@@ -28,21 +10,21 @@ describe('Puppeteer Test', () => {
 			defaultViewport: null,
 
 			// This is necessary to make `--load-extension` work
-			ignoreDefaultArgs: ['--disable-extensions'],
-			args: [
-				// '--disable-extensions-except=/home/alex/node/chrome-extension-react-typescript-3/dist',
-				'--load-extension=/home/alex/node/chrome-extension-react-typescript-3/dist',
-			],
+			// ignoreDefaultArgs: ['--disable-extensions'],
+			// args: [
+			// 	// '--disable-extensions-except=/home/alex/node/chrome-extension-react-typescript-3/dist',
+			// 	'--load-extension=/home/alex/node/chrome-extension-react-typescript-3/dist',
+			// ],
 		});
 		page = await browser.newPage();
+		await page.goto('http://localhost:5500/dist/dummy.html');
 	});
 
-	test('type test', async () => {
-		await page.goto('http://localhost:5500/dist/dummy.html');
-		await page.type('#keys input', 'test');
-		await page.waitForTimeout(2000);
-		const text = await page.$eval('#keys input', (el) => el.value);
-		expect(text).toBe('test');
+	test('`#keys` has display: flex after pressing `/`', async () => {
+		await page.waitForSelector('#keys');
+		await page.keyboard.press('/');
+		const display = await page.$eval('#keys', el => getComputedStyle(el).display);
+		expect(display).toBe('flex');
 	});
 
 	afterAll(async () => {
