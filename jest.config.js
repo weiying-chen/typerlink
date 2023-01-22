7a8da551341b613755919602dae93eb7d8309450
Alex <alexchen.info@gmail.com>
2023-01-22 21:23:59 +0800

    Fix input tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jest-environment-jsdom',
+  testEnvironment: 'jsdom',
   // setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect']
 };
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,25 @@
-import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { render, screen, fireEvent, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import React from 'react';
 import App from './App';
 
 describe('App Test', () => {
-	test('`#keys` has display: none on page load', async () => {
-        const app = render(<App />);
-        // const appElement = app.container.querySelector('#keys') 
-        // expect(appElement).toBe
-    });
+	test('`input` is not visible on page load', () => {
+		render(<App />);
+
+		const input = screen.getByRole('textbox', { hidden: true });
+
+		expect(input).not.toBeVisible();
+	});
+
+	test('`input` is visible after pressing `/`', () => {
+		render(<App />);
+		fireEvent.keyDown(document, { key: '/' });
+
+		const input = screen.getByRole('textbox', { hidden: true });
+
+		expect(input).toBeVisible();
+	});
 });
