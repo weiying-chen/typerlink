3bcd6f8896d9d9ccef3dc4dd88834a6683addf81
Alex <alexchen.info@gmail.com>
2023-01-25 23:16:14 +0800

    Create Dummy

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,9 +4,13 @@ import userEvent from '@testing-library/user-event';
 
 import React from 'react';
 import App from './App';
+import Dummy from './__mocks__/Dummy';
 
-describe('App Test', () => {
-	test('`input` is not visible on page load', () => {
+// Otherwise, the `scrollIntoView` called from `App` will throw an error.
+Element.prototype.scrollIntoView = jest.fn();
+
+describe('`input`', () => {
+	test('is not visible on page load', () => {
 		render(<App />);
 
 		const input = screen.getByRole('textbox', { hidden: true });
@@ -14,14 +18,39 @@ describe('App Test', () => {
 		expect(input).not.toBeVisible();
 	});
 
-	test('`input` is visible after pressing `/`', () => {
+	test('is visible after pressing `/`', () => {
 		render(<App />);
 		fireEvent.keyDown(document, { key: '/' });
 
-		const input = screen.getByRole('textbox', { hidden: true });
+		const input = screen.getByRole('textbox');
 
 		expect(input).toBeVisible();
 	});
+})
 
-	// TODO: Use the `container` option to inject dummy HTML? https://testing-library.com/docs/react-testing-library/api/#container
+describe('`button`', () => {
+	test('has been highlighted`', async () => {
+		const { container } = render(
+			<>
+				<Dummy />
+				<App />
+			</>
+		);
+
+		fireEvent.keyDown(document, { key: '/' });
+
+		const input = screen.getByRole('textbox');
+
+		await userEvent.type(input, 'B');
+
+		const button = screen.getByRole('button');
+
+		expect(container).toMatchSnapshot();
+		expect(button).toBeVisible();
+
+		// TODO: Ask ChatGPT
+		// 1. the best way to test <button><mark>B</mark></button> 
+		// 2. If jest.mock() should be used
+		// 3. If can change the `fireEvent`s to `userEvent`s. 
+	});
 });
---
+++ src/__mocks__/Dummy.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Dummy () {
+	return (
+		<div id="dummy">
+			<button>Button</button>
+		</div>
+	);
+};
+
+export default Dummy
